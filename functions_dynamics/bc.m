47f1f74b26de629e1157955c5ed7d83c20e8e3ae
Nicolas Cuny <nicolas.cuny@unige.ch>
2023-03-01 11:27:25 +0100

    Implement line tension at the boundary

--- functions_dynamics/bc.m
+++ functions_dynamics/bc.m
@@ -1,6 +1,6 @@
-function res = bc(yleft,yright, varpar,  U, dsU, C1, C2, C, dsC, Psi, X, Z, X0, L, L0, zeta, dszeta, zetac, zetanem, dszetanem, zetacnem, eta, etab, etacb, etap, xintegral, fext, kappa, K, xi, FixedPar, t, P0, thalf_P, tsigma) 
+function res = bc(yleft,yright, varpar,  U, dsU, C1, C2, C, dsC, Psi, X, Z, X0, L, L0, zeta, dszeta, zetac, zetanem, dszetanem, zetacnem, eta, etab, etacb, etap, xintegral, fext, kappa, K, xi, kb, x0b, FixedPar, t, P0, thalf_P, tsigma) 
 %v = (dsvs, vn, dsvn, mss, tns, dV(s), dX(s), vs, dsnew(s), I(s))
-%if mod(i,2)==0
+
 res = [ yleft(3) - 0.; %dsvn(0)=0
         yleft(5) - 0; %tns(0)=0
         yleft(6) - 0.; %v0(0)=0 
@@ -10,25 +10,10 @@ res = [ yleft(3) - 0.; %dsvn(0)=0
         yright(4) - 0; %mss(L)=0
         yright(6) - 0; %dV(L)=0
         yright(7) - 0;%(-1/xi)*(varpar(2)*xintegral); %dX0(L)=0
-        2*K*U(L) + zeta(L) - zetanem(L) + (eta+etab)*yright(1) + (etab-eta)*cos(Psi(L)).*yright(8)./X(L) + (etab*C(L)+eta*(C2(L)-C1(L))).*yright(2);%tss(L)=0
+        2*K*U(L) + zeta(L) - zetanem(L) + (eta+etab)*yright(1) + (etab-eta)*cos(Psi(L)).*yright(8)./X(L) + (etab*C(L)+eta*(C2(L)-C1(L))).*yright(2)+2*pi*kb*(1-x0b/X(L))*cos(Psi(L)); %tss(L)=2pi*k*(1-x0/x) cos(psi)
         yleft(10); %I1(0)=0
-        yright(5)]; %tns(L)=0 instead of I1(L)=0 
-        %yright(10)];
-% elseif mod(i,2)==1
-%     res = [ yleft(3) - 0.; %dsvn(0)=0
-%         yleft(5) - 0.; %tns(L)=0
-%         yleft(6) - 0.; %v0(0)=0 
-%         yleft(7) - 0.; %dX0(0)=0
-%         yleft(8) - 0.; %vs(0)=0
-%         yleft(9) - 0,; %dsnew(0) = 0
-%         yright(3) - 0; %dsvn(L)=0
-%         yright(6) - 0.; %dV=0
-%         yright(7) - 0.; %dX0=0
-%         yright(8) - 0;%vs(0)=0
-%         yleft(10); %I1(0)=0
-%         yright(10)]; %tns(L)=0 instead of I1(L)=0
-% end    
-    
+        yright(5)+2*pi*kb*(1-x0b/X(L))*sin(Psi(L))]; %tns(L)=2pi*k*(1-x0/x) sin(psi)
+
 if strcmp(FixedPar,'P')
     res(8) = yright(6)-varpar(1);
 end
--- functions_dynamics/intensity.m
+++
@@ -1,4 +0,0 @@
-function x = intensity(t,th,w)
-x = (1+exp(-w.*(t-th))).^(-1);
-end
-

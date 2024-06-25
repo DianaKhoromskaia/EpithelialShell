7030b67ccf7589caf23437a243bd7c2345473c76
Nicolas Cuny <nicolas.cuny@unige.ch>
2024-06-25 11:21:51 +0200

    Add zeta_c tension effect

--- functions_dynamics/ode.m
+++ functions_dynamics/ode.m
@@ -1,9 +1,10 @@
-function dvds = ode(s, v, varpar, U, dsU, C1, C2, C, C0, dsC, Psi, X, Z, X0, L, L0, zeta, dszeta, zetac, zetanem, dszetanem, zetacnem, eta, etab, etacb, etap, xintegral, fext, kappa, K, xi, kb, x0b, kp, psi0b, FixedPar, t, P0, thalf_P, tsigma)
+function dvds = ode(s, v, varpar, U, dsU, C1, C2, C, C0, dsC, Psi, X, Z, X0, L, L0, zeta, dszeta, zetac, dszetac, zetanem, dszetanem, zetacnem, eta, etab, etacb, etap, xintegral, fext, kappa, K, xi, kb, x0b, kp, psi0b, FixedPar, t, P0, thalf_P, tsigma)
 % rhs for ode for the vector v = (dsvs, vn, dsvn, mss, tns, dV(s), dX(s),
 % vs, dsnew(s), I(s)), solved on interval [0,L]
 %figure(60)
 %plot(s,v)
 %legend('dsvs', 'vn', 'dsvn', 'mss', 'tns', 'dV(s)', 'dX(s)','vs', 'dsnew(s)', 'I')
+
 
 if strcmp(FixedPar,'V')
     P = varpar(1);
@@ -12,6 +13,7 @@ elseif strcmp(FixedPar,'P')
 end
 
 fc=varpar(2);
+
 
 dsvs = v(1,:);
 vn = v(2,:);
@@ -33,11 +35,11 @@ onevec = ones(size(s));
 
 ds2vn = -cos(Psi(s)).*dsvn./X(s) - vn.*(C1(s).^2 + C2(s).^2) + vs.*dsC(s) - (mss - 2*kappa*(C(s)-C0) - zetac(s) + zetacnem(s))/etacb;
 
-tss = 2*K*U(s) + zeta(s) - zetanem(s) -2*kappa*C(s).*(C2(s)-0.5*C(s)) + (eta+etab)*dsvs + (etab-eta)*cos(Psi(s)).*vs./X(s) + (etab*C(s)+eta*(C2(s)-C1(s))).*vn;
-dstns = 2*C1(s).*(T+V-kappa*C(s).*(C1(s)-C2(s))) + (C1(s)+C2(s)).*tss - cos(Psi(s)).*tns./X(s) - P + cos(Psi(s)).*fc  - fextn;
+tss = 2*K*U(s) + zeta(s) - zetanem(s) -(2*kappa*C(s)+zetac(s)).*(C2(s)-0.5*C(s)) -C(s).*(kappa*C0-0.5*zetac(s)) + (eta+etab)*dsvs + (etab-eta)*cos(Psi(s)).*vs./X(s) + (etab*C(s)+eta*(C2(s)-C1(s))).*vn;
+dstns = 2*C1(s).*(T+V-(kappa*C(s)-kappa*C0+0.5*zetac(s)).*(C1(s)-C2(s))) + (C1(s)+C2(s)).*tss - cos(Psi(s)).*tns./X(s) - P + cos(Psi(s)).*fc  - fextn;
 dsmss = tns + 2*cos(Psi(s)).*zetacnem(s)./X(s);
 
-ds2vs = -cos(Psi(s)).*(dsvs-cos(Psi(s)).*vs./X(s))./X(s) - (eta-etab)*C1(s).*C2(s).*vs/(eta+etab) - dsC(s).*vn - (etab*C(s)+eta*(C2(s)-C1(s))).*dsvn/(eta+etab) - (2*K*dsU(s)+dszeta(s))/(eta+etab)  + (dszetanem(s) + 2*cos(Psi(s)).*zetanem(s)./X(s))/(eta+etab) - C2(s).*tns./(eta+etab) - sin(Psi(s)).*fc/(eta+etab) + 2*kappa*(dsC(s).*C2(s)-cos(Psi(s)).*C(s).*(C2(s)-C1(s)))./(eta+etab);
+ds2vs = -cos(Psi(s)).*(dsvs-cos(Psi(s)).*vs./X(s))./X(s) - (eta-etab)*C1(s).*C2(s).*vs/(eta+etab) - dsC(s).*vn - (etab*C(s)+eta*(C2(s)-C1(s))).*dsvn/(eta+etab) - (2*K*dsU(s)+dszeta(s))/(eta+etab)  + (dszetanem(s) + 2*cos(Psi(s)).*zetanem(s)./X(s))/(eta+etab) - C2(s).*tns./(eta+etab) - sin(Psi(s)).*fc/(eta+etab) + 2*kappa*(dsC(s).*C2(s)-cos(Psi(s)).*C(s).*(C2(s)-C1(s))./X(s))./(eta+etab)+ (cos(Psi(s)).*(2*kappa*C0-zetac(s)).*(C2(s)-C1(s))./X(s)-C1(s).*dszetac(s))./(eta+etab);
 
 
 % force balance within the interval:
@@ -61,12 +63,13 @@ dvds = [ds2vs;
             zervec;
             0.5*(-vn(indices).*(C1(0).^2 + C2(0).^2) - (mss(indices) - 2*kappa*(C(0)-C0) - zetac(0))/etacb);
             tns(indices);
-            -0.5*P*onevec+C2(0).*(2*K*U(0) + zeta(0) - zetanem(0) - 2*kappa*C(0)*(C2(0)-0.5*C(0)) + (eta+etab)*dsvs(indices) + etab*C(0)*vn(indices))+0.5*xi*vn(indices)+0.5*fc*onevec;%-0.5*P*onevec+C2(0).*tss(indices)+0.5*xi*vn(indices)+0.5*fc*onevec;
+            -0.5*P*onevec+C2(0).*(2*K*U(0) + zeta(0) - zetanem(0) - (2*kappa*C(0)- C0 + zetac(0))*(C2(0)-0.5*C(0)) + (eta+etab)*dsvs(indices) + etab*C(0)*vn(indices))+0.5*xi*vn(indices)+0.5*fc*onevec;%-0.5*P*onevec+C2(0).*tss(indices)+0.5*xi*vn(indices)+0.5*fc*onevec;
             zervec;
             zervec;
             dsvs(indices);%0.5*((tss(indices) - 2*K*U(0) - zeta(0))/etab - C(0).*vn(indices));
             dsvs(indices) + C2(0).*vn(indices);%sqrt(onevec+2*dt*(dsvs(indices) + C2(0).*vn(indices))+(dt^2)*((dsvs(indices) + C2(0).*vn(indices)).^2+(dsvn(indices)-C2(0).*vs(indices)).^2));%sqrt(onevec+dt*(tss(indices) - 2*K*U(0) - zeta(0))/etab+(dt^2)*(tss(indices) - 2*K*U(0) - zeta(0)).^2/(4*etab^2));%(0.5*((tss(indices) - 2*K*U(0) - zeta(0))/etab - C(0).*vn(indices))) + C2(0)*vn(indices);
             zervec];
+
 
  % specify force balance at NP:       
 %         indices = find(~(s-L));

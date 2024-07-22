870c583e8905b7c19f8dc1186fa4520b57e149a0
Nicolas Cuny <nicolas.cuny@unige.ch>
2024-07-22 11:36:46 +0200

    Remove steady-state files

--- functions_steadystate/ds0x0.m
+++
@@ -1,3 +0,0 @@
-function x = ds0x0(s0) %dim-less undeformed shape derivative ds0x0(s0)
-x = pi*cos(s0*pi);
-end
--- functions_steadystate/yguessfun_extrap.m
+++
@@ -1,6 +0,0 @@
-function y = yguessfun_extrap(varargin)  %(s, k, sol) % k-region
-s = varargin{1};%first argument
-sol = varargin{nargin}; %last argument
-    
-y = deval(sol, s);
-end

a12d417d09da44fab0d21395bc5577b01185cee0
Nicolas Cuny <nicolas.cuny@unige.ch>
2024-09-30 13:56:05 +0200

    Corrected boundary conditions

--- functions_dynamics/bc.m
+++ functions_dynamics/bc.m
@@ -2,7 +2,7 @@ function res = bc(yleft,yright, varpar,  U, dsU, C1, C2, C, C0, dsC, Psi, X, Z, 
 %v = (dsvs, vn, dsvn, mss, tns, dV(s), dX(s), vs, dsnew(s), I(s))
 
 tssL=2*K*U(L) - (kappa(L)*(C(L)-C0)+0.5*zetac(L))*(C2(L)-C1(L)) + zeta(L) - zetanem(L) + (eta+etab)*yright(1) + (etab-eta)*cos(Psi(L))*yright(8)/X(L) + (etab*C(L)+eta*(C2(L)-C1(L)))*yright(2) + (0.5*zetac(L)-kappa(L)*C0)*(C(L)-C0+0.5*zetac(L)/kappa(L));
-Cfs= (2*pi*kb*(1-x0b/X(L)) + 0.5*kp*(Psi(L)-psi0b)^2)/X(L);
+Cfs= (2*pi*kb*(X(L)-x0b) + 0.5*kp*(Psi(L)-psi0b)^2)/X(L);
 
 res = [ yleft(3) - 0.; %dsvn(0)=0
         yleft(5) - 0; %tns(0)=0
@@ -13,9 +13,9 @@ res = [ yleft(3) - 0.; %dsvn(0)=0
         yright(4) + kp*(Psi(L)-psi0b); %mss(L) open boundary
         yright(6) - 0; %dV(L)=0
         yright(7) - 0;%(-1/xi)*(varpar(2)*xintegral); %dX0(L)=0
-        Cfs + tssL*cos(Psi(L)) + yright(5)*sin(Psi(L)); %tss open boundary
+        tssL + Cfs*cos(Psi(L)); %tss open boundary
         yleft(10); %I1(0)=0
-        tssL*sin(Psi(L)) - yright(5)*cos(Psi(L))]; %tns(L) open boundary
+        yright(5) + Cfs*sin(Psi(L))]; %tns(L) open boundary
 
 
 if strcmp(FixedPar,'P')
